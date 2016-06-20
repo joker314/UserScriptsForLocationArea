7f7850590ca95a7d4779ba3112c74a0f75f6e70c
joker314 <maxzuba22@yahoo.co.uk>
2016-06-20 17:21:58 +0100

    Create contenteditable.user.js

---
+++ contenteditable.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        UserScriptsForLocationArea_1
+// @namespace   https://joker314.github.io/
+// @description Makes location section on Scratch profile content editable.
+// @include     *://scratch.mit.edu/user/*
+// @version     0.1
+// ==/UserScript==
+document.getElementsByClassName("location")[0].contentEditabe = true;
